9fd1e0768b1a5197fca89ad69a813b9131f059ad
刘辉20020013 <20020013@suning.com>
2023-06-17 23:47:46 +0800

    feat: fix error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:jd_mall_flutter/main_prd.dart';
+import 'package:jd_mall_flutter/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MallApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
