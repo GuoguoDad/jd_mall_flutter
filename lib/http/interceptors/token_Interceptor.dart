6661b2ede4aa6ae2b9087c8e23e8872bfd8b5f05
刘辉20020013 <20020013@suning.com>
2024-01-26 10:51:24 +0800

    feat: 登录

--- lib/http/interceptors/token_Interceptor.dart
+++ lib/http/interceptors/token_Interceptor.dart
@@ -1,20 +1,20 @@
 // Package imports:
 import 'package:dio/dio.dart';
 
+import 'package:jd_mall_flutter/common/util/local_storage_util.dart';
+
 class TokenInterceptors extends InterceptorsWrapper {
   String? _token;
 
   @override
   onRequest(RequestOptions options, handler) async {
-    //授权码
     if (_token == null) {
-      // var authorizationCode = await getAuthorization();
-      // if (authorizationCode != null) {
-      //   _token = authorizationCode;
-      //   await initClient(_token);
-      // }
+      var authorizationCode = await LocalStorageUtil.getString("token");
+      if (authorizationCode != null) {
+        _token = authorizationCode;
+      }
     }
-    if(_token != null) {
+    if (_token != null) {
       options.headers["Authorization"] = _token;
     }
     return super.onRequest(options, handler);
@@ -25,8 +25,8 @@ class TokenInterceptors extends InterceptorsWrapper {
     try {
       var responseJson = response.data;
       if (response.statusCode == 201 && responseJson["token"] != null) {
-        _token = 'token ' + responseJson["token"];
-        // await LocalStorage.save(Config.TOKEN_KEY, _token);
+        _token = responseJson["token"];
+        await LocalStorageUtil.save("token", _token);
       }
     } catch (e) {
       print(e);
