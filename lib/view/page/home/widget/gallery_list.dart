2c04e97392c98780bd0bb08cfcdc70b72f239e3d
刘辉20020013 <447092991@qq.com>
2026-05-29 11:38:52 +0800

    feat: 更换首页滚动图片组件

--- lib/view/page/home/widget/gallery_list.dart
+++ lib/view/page/home/widget/gallery_list.dart
@@ -1,10 +1,12 @@
 // Flutter imports:
 import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 // Package imports:
 import 'package:get/get.dart';
 import 'package:jd_mall_flutter/component/indicator/common_indicator.dart';
+import 'package:jd_mall_flutter/models/home_page_info.dart';
 import 'package:provider/provider.dart';
 
 // Project imports:
@@ -35,59 +37,104 @@ class GalleryList extends StatelessWidget {
             borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8)),
           ),
           child: Consumer<HomeProvider>(
-            builder: (context, provider, child) {
-              var bannerList = provider.homePageInfo.bannerList ?? [];
+              builder: (context, provider, child) {
+                var bannerList = provider.homePageInfo.bannerList ?? [];
 
-              if(bannerList.isEmpty) return Container();
+                if(bannerList.isEmpty) return Container();
 
-              return Stack(
-                children: [
-                  ExpandablePageView.builder(
-                    loop: true,
-                    itemCount: bannerList.length,
-                    onPageChanged: (index) {
-                      provider.setImgSliderIndex(index);
-                    },
-                    itemBuilder: (BuildContext context, int index) {
-
-                      return GestureDetector(
-                        onTap: () => Get.toNamed(RoutesEnum.detailPage.path),
-                        child: Container(
-                          margin: const EdgeInsets.fromLTRB(10, 10, 10, 2),
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.all(Radius.circular(6)),
-                            child: ExtendImageNetwork(url: bannerList[index].imgUrl!,
-                              width: carouselWidth,
-                              height: carouselHeight,
-                              cache: true,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 8,
-                    child: Container(
-                      height: 10,
-                      width: double.infinity,
-                      alignment: Alignment.center,
-                      child: CommonIndicator(
-                        itemCount: bannerList.length,
-                        current: provider.imgSliderIndex,
-                      ),
-                    ),
-                  ),
-                ],
-              );
-
-            }
+                return CarouselSlider(bannerList);
+              }
           ),
         ),
       ),
+    );
+  }
+}
+
+class CarouselSlider extends StatefulWidget {
+  final List<BannerList> bannerList;
+  const CarouselSlider(this.bannerList, {super.key});
+
+  @override
+  State<CarouselSlider> createState() => _CarouselSliderState();
+}
+
+class _CarouselSliderState extends State<CarouselSlider> {
+  late final controller = ExpandablePageController(itemCount: widget.bannerList.length);
+  late Timer _timer;
+
+  int activeIndex = 0;
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 8), (timer) {
+      if (controller.hasClients) {
+        int nextPage = (activeIndex + 1) % widget.bannerList.length;
+        controller.animateToPage(
+          nextPage,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut
+        );
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ExpandablePageView.builder(
+          loop: true,
+          controller: controller,
+          itemCount: widget.bannerList.length,
+          onPageChanged: (index) {
+            setState(() { activeIndex = index; });
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () => Get.toNamed(RoutesEnum.detailPage.path),
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(10, 10, 10, 2),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(6)),
+                  child: ExtendImageNetwork(url: widget.bannerList[index].imgUrl!,
+                    width: carouselWidth,
+                    height: carouselHeight,
+                    cache: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 8,
+          child: Container(
+            height: 10,
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: CommonIndicator(
+              itemCount: widget.bannerList.length,
+              current: activeIndex,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
